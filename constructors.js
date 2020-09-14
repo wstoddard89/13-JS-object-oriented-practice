0b80cc1a5a644cc2b176ec45c7fde5ce280237f4
Will Stoddard <wstoddard89@gmail.com>
2020-09-14 16:43:20 -0700

    Answer all questions correctly. Finish assignment

--- constructors.js
+++ constructors.js
@@ -19,21 +19,21 @@ function expect(target) {
     toBe: function (expectation) {
       if (target === expectation) {
         console.log(
-          '\n     %cPASSED',
-          'color:green;',
-          'Expected',
+          "\n     %cPASSED",
+          "color:green;",
+          "Expected",
           target,
-          'to be',
+          "to be",
           expectation
         )
         return true
       } else {
         console.log(
-          '\n     %cFAILED',
-          'color:red;',
-          'Expected',
+          "\n     %cFAILED",
+          "color:red;",
+          "Expected",
           target,
-          'to be',
+          "to be",
           expectation
         )
         return false
@@ -50,24 +50,34 @@ function expect(target) {
 //
 // Only add code to *THIS* section!
 
-function Dog() {
-  this.status = 'normal';
-  this.color = 'black';
+function Dog(obj) {
+  this.status = "normal"
   this.hungry = true
+
+  if (obj === undefined) {
+    this.color = null
+  } else {
+    this.color = obj.color
+  }
+
+  if (obj !== undefined && obj.hungry !== undefined) {
+    this.hungry = obj.hungry
+  }
 }
-Human.prototype.pet = function() {
-  sadie.status = 'happy';
-  sadie.hungry = false;
-  mason.cool = false;
+Human.prototype.pet = function (obj) {
+  obj.status = "happy"
 }
 
-Human.prototype.feed = function() {
-  moonshine.hungry = false;
+Human.prototype.feed = function (obj) {
+  obj.hungry = false
 }
 
-
-function Human() {
-  this.cool = true;
+function Human(obj) {
+  if (obj === undefined) {
+    this.cool = false
+  } else {
+    this.cool = obj.cool
+  }
 }
 
 //        __
@@ -78,12 +88,12 @@ function Human() {
 //            /____/
 
 var sadie = new Dog({
-  color: 'black',
+  color: "black",
   hungry: false,
 })
 
 var moonshine = new Dog({
-  color: 'blue-red',
+  color: "blue-red",
 })
 
 var atticus = new Dog()
@@ -109,22 +119,22 @@ var julia = new Human({
 // Don't edit this section. Instead make these tests pass by writing
 // constructors in the constructor section above ;D
 
-it('should make Sadie happy when Mason pets her', function () {
-  expect(sadie.status).toBe('normal')
+it("should make Sadie happy when Mason pets her", function () {
+  expect(sadie.status).toBe("normal")
   mason.pet(sadie)
-  expect(sadie.status).toBe('happy')
+  expect(sadie.status).toBe("happy")
 })
 
-it('should make Sadie black', function () {
-  expect(sadie.color).toBe('black')
+it("should make Sadie black", function () {
+  expect(sadie.color).toBe("black")
 })
 
-it('should be make Moonshine hungry and Sadie not hungry', function () {
+it("should be make Moonshine hungry and Sadie not hungry", function () {
   expect(moonshine.hungry).toBe(true)
   expect(sadie.hungry).toBe(false)
 })
 
-it('should make Moonshine no longer hungry when you feed him', function () {
+it("should make Moonshine no longer hungry when you feed him", function () {
   julia.feed(moonshine)
   expect(moonshine.hungry).toBe(false)
 })
@@ -135,7 +145,7 @@ it("should not affect Atticus and Moonshine's owner properties when setting Maso
   expect(atticus.owner).toBe(undefined)
 })
 
-it('should make Julia cool and Mason not cool', function () {
+it("should make Julia cool and Mason not cool", function () {
   sadie.owner = mason
   expect(julia.cool).toBe(true)
   expect(mason.cool).toBe(false)
